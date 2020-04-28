a62d2f51587672da477a7fbda269a29b3d2ad90a
alexwatts83 <alexjwatts83@gmail.com>
2020-04-28 11:17:07 +1000

    added very basic .net core web app

---
+++ WebAdvert.Web/Models/SignupModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Emaill")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(8, ErrorMessage = "Password must be 8 characers long.")]
+        public string Pasword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
